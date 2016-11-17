87ea2243db78fe18d265af9869f365c057c117a6
bayuRP <13513046@std.stei.itb.ac.id>
2016-11-17 14:40:18 +0700

    Initialize MyAgnes

--- Kelastering/src/kelastering/MyAgnes.java
+++ Kelastering/src/kelastering/MyAgnes.java
@@ -5,10 +5,35 @@
  */
 package kelastering;
 
+import weka.clusterers.AbstractClusterer;
+import weka.core.Instances;
+
 /**
  *
  * @author ryanyonata
  */
-public class MyAgnes {
+public class MyAgnes extends AbstractClusterer {
+    
+    final static int SINGLE = 0;
+    final static int COMPLETE = 0;
+    
+    Instances m_instances;
+    int m_clusters = 2;
+    int m_linkType = SINGLE;
+
+    public void AbstractClusterer (int clusters, int linkType) {
+        m_clusters = clusters;
+        m_linkType = linkType;
+    }
+    
+    @Override
+    public void buildClusterer(Instances i) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int numberOfClusters() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
