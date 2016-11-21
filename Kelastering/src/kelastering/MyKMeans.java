f01c112921caf3ad892693a2c0f5dc2e79e83962
Julio Savigny <jsavigny@gmail.com>
2016-11-21 16:13:18 +0700

    add toString kmeans

--- Kelastering/src/kelastering/MyKMeans.java
+++ Kelastering/src/kelastering/MyKMeans.java
@@ -6,10 +6,7 @@
 package kelastering;
 import weka.clusterers.AbstractClusterer;
 import weka.clusterers.RandomizableClusterer;
-import weka.core.DistanceFunction;
-import weka.core.EuclideanDistance;
-import weka.core.Instance;
-import weka.core.Instances;
+import weka.core.*;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -25,6 +22,9 @@ public class MyKMeans extends RandomizableClusterer {
     protected DistanceFunction distanceFunction = new EuclideanDistance();
     private double [] squaredErrors;
     private int[] assignment;
+    private int maxIterations = 500;
+    private int iterations;
+    private int[] clusterSize;
 
     public MyKMeans(){
         super();
@@ -33,9 +33,14 @@ public class MyKMeans extends RandomizableClusterer {
         numCluster = 2; // Default = 2
     }
 
-    public MyKMeans(int numCluster){
-        this.numCluster = numCluster;
+    public int getMaxIterations() {
+        return maxIterations;
     }
+
+    public void setMaxIterations(int maxIterations) {
+        this.maxIterations = maxIterations;
+    }
+
 
     public void setNumClusters(int n) throws Exception {
         if (n <= 0) {
@@ -76,8 +81,12 @@ public class MyKMeans extends RandomizableClusterer {
         Instances [] tempClusters = new Instances[numCluster];
 
         boolean isConverged = false;
+
+        iterations = 0;
         while (!isConverged) {
             isConverged = true;
+            iterations++;
+            System.out.println(iterations);
             for (int i = 0; i < instanceSize; i++){
                 Instance toBeClustered = instances.instance(i);
                 int newCluster = clusterProcessedInstance(toBeClustered,true);
@@ -99,6 +108,17 @@ public class MyKMeans extends RandomizableClusterer {
                 moveCentroid(tempClusters[i]);
             }
 
+            if (!isConverged){
+                squaredErrors = new double[numCluster];
+            }
+
+            if (iterations == maxIterations){
+                isConverged = true;
+            }
+            clusterSize = new int [numCluster];
+            for (int i = 0; i < numCluster; i++){
+                clusterSize[i] = tempClusters[i].numInstances();
+            }
         }
 
     }
@@ -141,5 +161,197 @@ public class MyKMeans extends RandomizableClusterer {
         centroids.add(new Instance(1.0, vals));
 
         return vals;
+    }
+
+    public String toString() {
+        if (centroids == null) {
+            return "No clusterer built yet!";
+        }
+
+        int maxWidth = 0;
+        int maxAttWidth = 0;
+        boolean containsNumeric = false;
+        for (int i = 0; i < numCluster; i++) {
+            for (int j = 0 ;j < centroids.numAttributes(); j++) {
+                if (centroids.attribute(j).name().length() > maxAttWidth) {
+                    maxAttWidth = centroids.attribute(j).name().length();
+                }
+                if (centroids.attribute(j).isNumeric()) {
+                    containsNumeric = true;
+                    double width = Math.log(Math.abs(centroids.instance(i).value(j))) /
+                            Math.log(10.0);
+                    //          System.err.println(m_ClusterCentroids.instance(i).value(j)+" "+width);
+                    if (width < 0) {
+                        width = 1;
+                    }
+                    // decimal + # decimal places + 1
+                    width += 6.0;
+                    if ((int)width > maxWidth) {
+                        maxWidth = (int)width;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < centroids.numAttributes(); i++) {
+            if (centroids.attribute(i).isNominal()) {
+                Attribute a = centroids.attribute(i);
+                for (int j = 0; j < centroids.numInstances(); j++) {
+                    String val = a.value((int)centroids.instance(j).value(i));
+                    if (val.length() > maxWidth) {
+                        maxWidth = val.length();
+                    }
+                }
+                for (int j = 0; j < a.numValues(); j++) {
+                    String val = a.value(j) + " ";
+                    if (val.length() > maxAttWidth) {
+                        maxAttWidth = val.length();
+                    }
+                }
+            }
+        }
+
+        // check for size of cluster sizes
+        for (int i = 0; i < clusterSize.length; i++) {
+            String size = "(" + clusterSize[i] + ")";
+            if (size.length() > maxWidth) {
+                maxWidth = size.length();
+            }
+        }
+
+        String plusMinus = "+/-";
+        maxAttWidth += 2;
+        if (maxAttWidth < "Attribute".length() + 2) {
+            maxAttWidth = "Attribute".length() + 2;
+        }
+
+        if (maxWidth < "Full Data".length()) {
+            maxWidth = "Full Data".length() + 1;
+        }
+
+        if (maxWidth < "missing".length()) {
+            maxWidth = "missing".length() + 1;
+        }
+
+
+
+        StringBuffer temp = new StringBuffer();
+        //    String naString = "N/A";
+
+
+    /*    for (int i = 0; i < maxWidth+2; i++) {
+          naString += " ";
+          } */
+        temp.append("\nkMeans\n======\n");
+        temp.append("\nNumber of iterations: " + iterations+"\n");
+
+        if(distanceFunction instanceof EuclideanDistance){
+            temp.append("Within cluster sum of squared errors: " + Utils.sum(squaredErrors));
+        }else{
+            temp.append("Sum of within cluster distances: " + Utils.sum(squaredErrors));
+        }
+
+        temp.append("\n\nCluster centroids:\n");
+        temp.append(pad("Cluster#", " ", (maxAttWidth + (maxWidth * 2 + 2)) - "Cluster#".length(), true));
+
+        temp.append("\n");
+        temp.append(pad("Attribute", " ", maxAttWidth - "Attribute".length(), false));
+
+
+//        temp.append(pad("Full Data", " ", maxWidth + 1 - "Full Data".length(), true));
+
+        // cluster numbers
+        for (int i = 0; i < numCluster; i++) {
+            String clustNum = "" + i;
+            temp.append(pad(clustNum, " ", maxWidth + 1 - clustNum.length(), true));
+        }
+        temp.append("\n");
+
+        // cluster sizes
+        String cSize = "";
+        temp.append(pad(cSize, " ", maxAttWidth - cSize.length(), true));
+        for (int i = 0; i < numCluster; i++) {
+            cSize = "(" + clusterSize[i] + ")";
+            temp.append(pad(cSize, " ",maxWidth + 1 - cSize.length(), true));
+        }
+        temp.append("\n");
+
+        temp.append(pad("", "=", maxAttWidth +
+                (maxWidth * (centroids.numInstances())
+                        + centroids.numInstances()), true));
+        temp.append("\n");
+
+        for (int i = 0; i < centroids.numAttributes(); i++) {
+            String attName = centroids.attribute(i).name();
+            temp.append(attName);
+            for (int j = 0; j < maxAttWidth - attName.length(); j++) {
+                temp.append(" ");
+            }
+
+            String strVal;
+            String valMeanMode;
+            // full data
+//            if (centroids.attribute(i).isNominal()) {
+//                if (m_FullMeansOrMediansOrModes[i] == -1) { // missing
+//                    valMeanMode = pad("missing", " ", maxWidth + 1 - "missing".length(), true);
+//                } else {
+//                    valMeanMode =
+//                            pad((strVal = centroids.attribute(i).value((int)m_FullMeansOrMediansOrModes[i])),
+//                                    " ", maxWidth + 1 - strVal.length(), true);
+//                }
+//            } else {
+//                if (Double.isNaN(m_FullMeansOrMediansOrModes[i])) {
+//                    valMeanMode = pad("missing", " ", maxWidth + 1 - "missing".length(), true);
+//                } else {
+//                    valMeanMode =  pad((strVal = Utils.doubleToString(m_FullMeansOrMediansOrModes[i],
+//                            maxWidth,4).trim()),
+//                            " ", maxWidth + 1 - strVal.length(), true);
+//                }
+//            }
+//            temp.append(valMeanMode);
+
+            for (int j = 0; j < numCluster; j++) {
+                if (centroids.attribute(i).isNominal()) {
+                    if (centroids.instance(j).isMissing(i)) {
+                        valMeanMode = pad("missing", " ", maxWidth + 1 - "missing".length(), true);
+                    } else {
+                        valMeanMode =
+                                pad((strVal = centroids.attribute(i).value((int)centroids.instance(j).value(i))),
+                                        " ", maxWidth + 1 - strVal.length(), true);
+                    }
+                } else {
+                    if (centroids.instance(j).isMissing(i)) {
+                        valMeanMode = pad("missing", " ", maxWidth + 1 - "missing".length(), true);
+                    } else {
+                        valMeanMode = pad((strVal = Utils.doubleToString(centroids.instance(j).value(i),
+                                maxWidth,4).trim()),
+                                " ", maxWidth + 1 - strVal.length(), true);
+                    }
+                }
+                temp.append(valMeanMode);
+            }
+            temp.append("\n");
+        }
+
+        temp.append("\n\n");
+        return temp.toString();
+    }
+
+    private String pad(String source, String padChar,
+                       int length, boolean leftPad) {
+        StringBuffer temp = new StringBuffer();
+
+        if (leftPad) {
+            for (int i = 0; i< length; i++) {
+                temp.append(padChar);
+            }
+            temp.append(source);
+        } else {
+            temp.append(source);
+            for (int i = 0; i< length; i++) {
+                temp.append(padChar);
+            }
+        }
+        return temp.toString();
     }
 }
